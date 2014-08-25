acc2aa8a40cb7cd04973128e7dbcee990529c91b
Aaron Bedra <aaron@aaronbedra.com>
2014-08-24 20:18:08 -0500

    Adding simple request handling

--- src/main/java/HTTPServer.java
+++ src/main/java/HTTPServer.java
@@ -6,8 +6,10 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class HTTPServer {
+    private final static String[] supportedMethods = {"GET", "POST"};
 
     protected static void start(final int port) {
         System.out.println("Starting server on port " + port);
@@ -34,11 +36,16 @@ public class HTTPServer {
                 Socket remote = socket.accept();
                 System.out.println("Received connection, sending response");
                 BufferedReader in = new BufferedReader(new InputStreamReader(remote.getInputStream()));
+
                 String input = in.readLine();
+                String response = handle(remote, getURI(input));
+
                 while (!input.equals("")) {
                     input = in.readLine();
+                    System.out.println(input);
                 }
-                writeResponse(remote);
+
+                writeResponse(remote, response);
                 remote.close();
             } catch (IOException e) {
                 System.out.println("Error :" + e.getMessage());
@@ -46,7 +53,7 @@ public class HTTPServer {
         }
     }
 
-    private static void writeResponse(final Socket socket) {
+    private static void writeResponse(final Socket socket, final String response) {
         try {
             PrintWriter out = new PrintWriter(socket.getOutputStream());
 
@@ -55,10 +62,37 @@ public class HTTPServer {
             out.println("Server: Example");
             out.println("");
 
-            out.println("<h1>Hello Strangeloop</h1>");
+            out.println(response);
             out.flush();
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private static String getURI(final String queryString) {
+        String[] params = queryString.split(" ");
+
+        if (Arrays.asList(supportedMethods).contains(params[0])) {
+            return params[1];
+        }
+
+        return "";
+    }
+
+    private static String handle(final Socket socket, final String uri) {
+        String[] parts = uri.split("/");
+
+        if (parts.length > 1) {
+            switch (uri.split("/")[1]) {
+                case "encrypt":
+                    return "encrypted output";
+                case "decrypt":
+                    return "decrypted output";
+                default:
+                    return "<h1>Not Found</h1>";
+            }
+        } else {
+            return "<h1>Hello Strangeloop</h1>";
         }
     }
 }
