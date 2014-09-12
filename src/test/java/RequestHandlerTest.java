303f53efdc1c8442ca266cb351192f30c8bfe3f3
Aaron Bedra <aaron@aaronbedra.com>
2014-09-11 22:50:00 -0500

    Mock crypto for handler tests. Closes #8.

--- src/test/java/RequestHandlerTest.java
+++ src/test/java/RequestHandlerTest.java
@@ -1,12 +1,26 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.Base64;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@PrepareForTest(Crypto.class)
+@RunWith(PowerMockRunner.class)
 
 public class RequestHandlerTest {
+    byte[] textBytes = "testing".getBytes();
+    String encodedText = Base64.getEncoder().encodeToString("testing".getBytes());
     HTTPResponse response;
 
     @Before
     public void setUp() {
+        PowerMockito.mockStatic(Crypto.class);
         response = new HTTPResponse();
     }
 
@@ -22,27 +36,29 @@ public class RequestHandlerTest {
     }
 
     @Test
-    public void encryptPostRequest() {
+    public void encryptPostRequest() throws CryptoException {
+        when(Crypto.encrypt(textBytes)).thenReturn(textBytes);
         HTTPRequest request = new HTTPRequest("POST /encrypt HTTP/1.1");
-        request.setPostData("testing".getBytes());
+        request.setPostData(textBytes);
         RequestHandler handler = new RequestHandler(request, response);
         handler.handleRequest();
 
         assertEquals(HTTPResponse.OK, response.getStatus());
         assertEquals("OK", response.getStatusMessage());
-        assertEquals("encrypted data", response.getContent());
+        assertEquals(encodedText, response.getContent());
     }
 
     @Test
-    public void decryptPostRequest() {
+    public void decryptPostRequest() throws CryptoException {
+        when(Crypto.decrypt(textBytes)).thenReturn(textBytes);
         HTTPRequest request = new HTTPRequest("POST /decrypt HTTP/1.1");
         RequestHandler handler = new RequestHandler(request, response);
-        request.setPostData("testing".getBytes());
+        request.setPostData(encodedText.getBytes());
         handler.handleRequest();
 
         assertEquals(HTTPResponse.OK, response.getStatus());
         assertEquals("OK", response.getStatusMessage());
-        assertEquals("decrypted data", response.getContent());
+        assertEquals("testing", response.getContent());
     }
 
     @Test
